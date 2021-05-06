44ec7519ffc2323121fcf52d493188112a3cba57
unknown <prophreaker@gmail.com>
2021-05-06 17:07:09 +0300

    init: config + initial setup

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    '@babel/preset-env',
+  ];
+  const plugins = ['@babel/syntax-dynamic-import'];
+
+  return {
+    presets,
+    plugins,
+  };
+};
